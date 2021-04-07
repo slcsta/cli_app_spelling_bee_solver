abda05f7fe0c29e0560fc9fc51ee3477230824af
Sarah <sarahlchavez@gmail.com>
2021-04-07 16:08:55 -0700

    removed API endpoint

--- lib/spelling_bee_solver_cli/api.rb
+++ lib/spelling_bee_solver_cli/api.rb
@@ -1,9 +1,11 @@
 class API
     def self.get_data
-        response = RestClient.get("https://www.dictionaryapi.com/api/v3/references/sd4/json/test?key=71fb414c-9813-4244-8783-c3ab6da37a15")
-        words_array = JSON.parse(response)["results"]
-        words_array.each do |word|
-        Words.new(words)
+        response = RestClient.get("")
+        #binding.pry
+        # words_array = JSON.parse(response)[""]
+        # words_array.each do |word|
+        #    Words.new(words)
+        end
     end
 
 end
