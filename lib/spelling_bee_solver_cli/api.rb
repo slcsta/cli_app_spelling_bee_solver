39126a2bb003e558c2bbf65505ce76b050d34b39
Sarah <sarahlchavez@gmail.com>
2021-04-12 10:00:51 -0700

    upload

--- lib/spelling_bee_solver_cli/api.rb
+++ lib/spelling_bee_solver_cli/api.rb
@@ -1,11 +1,14 @@
 class API
     def self.get_data
-        response = RestClient.get("")
-        #binding.pry
+        
+        request["x-rapidapi-host"] = 'wordsapiv1.p.rapidapi.com'
+        request["x-rapid-key"] = ENV['WORDS_API_KEY']
+        # response = RestClient.get "https://wordsapiv1.p.key.com/words/?letterPattern=(?=.*e)(^[eginpux]{4,}$)" 
+        
+        binding.pry
         # words_array = JSON.parse(response)[""]
         # words_array.each do |word|
         #    Words.new(words)
-        end
     end
 
 end
